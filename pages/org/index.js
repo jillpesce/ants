4af70b9e68c0f9ae5f7a43ede759cd0fb2f5c912
oodwyer <orodwyer@gmail.com>
2021-04-01 20:48:03 -0400

    edit post frontend

--- pages/org/index.js
+++ pages/org/index.js
@@ -68,6 +68,7 @@ export default function Home() {
                             setCreate(false)
                             fetchPosts()
                         }}
+                        edit={false}
                     />
                 ) : (
                     <>
--- pages/post/[id].js
+++ pages/post/[id].js
@@ -6,11 +6,14 @@ import Post from '../../components/Post'
 import Header from '../../components/Header'
 import { useRouter } from 'next/router'
 import moment from 'moment'
+import CreatePost from '../../components/CreatePost'
+
 
 export default function PostPage() {
     const { user, userType, updateUser, logout } = useContext(UserContext)
     const [post, setPost] = useState()
     const [err, setErr] = useState()
+    const [update, setUpdate] = useState()
     const router = useRouter()
 
     //get post content
@@ -168,6 +171,24 @@ export default function PostPage() {
                             </button>
                         </div>
                     )}
+                    {
+                        userType !== 'user' && 
+                            (update ? 
+                            <CreatePost
+                                orgid={user._id}
+                                close={() => {
+                                setUpdate(false)}}
+                                postValues = {post}
+                                edit = {true}
+                    /> : 
+                            <button
+                                className="button yellow"
+                                onClick={() => setUpdate(true)}
+                            >
+                                Edit
+                            </button> )
+                    }
+
                     <b>{post.likes.length || 0} Likes</b>
                 </div>
             </div>
